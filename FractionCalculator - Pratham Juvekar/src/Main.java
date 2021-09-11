8a948b1a8d27bd977d6c7bd7d8a07deeb5202413
Pratham Juvekar <pratham.p.juvekar@gmail.com>
2021-09-11 15:24:45 -0400

    Initial Fraction Calculator project setup

---
+++ FractionCalculator - Pratham Juvekar/src/Main.java
@@ -0,0 +1,16 @@
+// Pratham Juvekar
+// APCS Block E
+// 9/10/2021
+// Fraction Calculator
+
+import javax.swing.*;
+import BreezySwing.*;
+
+public class Main extends GBFrame {
+    public static void main(String[] args) {
+
+
+        System.out.println("Test");
+
+    }
+}
