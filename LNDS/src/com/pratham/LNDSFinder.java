5f856160a25ee31895b9225b3ff3aba947ac56b5
Pratham Juvekar <pratham.p.juvekar@gmail.com>
2021-11-02 21:43:11 -0400

    Final version of LNDS

--- LNDS/src/com/pratham/LNDSFinder.java
+++ LNDS/src/com/pratham/LNDSFinder.java
@@ -2,6 +2,7 @@ package com.pratham;
 
 public class LNDSFinder {
 
+    //find all longest non-decreasing sequences
     static NumberArray[] findLNDS(NumberArray numbers) {
 
         if ( numbers.getLength() == 0 ) {
@@ -21,7 +22,7 @@ public class LNDSFinder {
                 break; //end of the array
             }
             if ( array[i] > array[i+1] ) {
-                ndsArrayIndex++; //end of nds
+                ndsArrayIndex++; //end of non-decreasing sequence
             }
         }
 
@@ -64,5 +65,70 @@ public class LNDSFinder {
         }
 
         return lndsArray;
+    }
+
+    //find all longest decreasing sequences
+    static NumberArray[] findLDS(NumberArray numbers) {
+
+        if ( numbers.getLength() == 0 ) {
+            return null; //no numbers
+        }
+
+        //find all decreasing sequences
+        NumberArray[] dsArray = new NumberArray[numbers.getLength()];
+        int dsArrayIndex = 0;
+        int[] array = numbers.getArray();
+        for ( int i=0; i< array.length; i++ ){
+            if ( dsArray[dsArrayIndex] == null ) {
+                dsArray[dsArrayIndex] = new NumberArray();
+            }
+            dsArray[dsArrayIndex].addNumber(array[i]);
+            if ( i+1 >= array.length ) {
+                break; //end of the array
+            }
+            if ( array[i] < array[i+1] ) {
+                dsArrayIndex++; //end of decreasing sequence
+            }
+        }
+
+        //find the length of longest sequence
+        int longest = dsArray[0].getLength();
+        for ( int i=0; i<dsArray.length; i++ ){
+            if ( dsArray[i] == null ) {
+                break;
+            }
+            if ( dsArray[i].getLength() > longest ) {
+                longest = dsArray[i].getLength();
+            }
+        }
+        if ( longest == 1 ) {
+            return null; // no LDS found
+        }
+
+        //find how many longest sequences are there
+        int longestCount = 0;
+        for ( int i=0; i<dsArray.length; i++ ){
+            if ( dsArray[i] == null ) {
+                break;
+            }
+            if ( dsArray[i].getLength() == longest ) {
+                longestCount++;
+            }
+        }
+
+        //return only the longest decreasing sequences
+        NumberArray[] ldsArray = new NumberArray[longestCount];
+        int ldsArrayIndex = 0;
+        for ( int i=0; i<dsArray.length; i++ ) {
+            if ( dsArray[i] == null ) {
+                break;
+            }
+            if ( dsArray[i].getLength() == longest ) {
+                ldsArray[ldsArrayIndex] = dsArray[i];
+                ldsArrayIndex++;
+            }
+        }
+
+        return ldsArray;
     }
 }
--- LNDS/src/com/pratham/LNDSGui.java
+++ LNDS/src/com/pratham/LNDSGui.java
@@ -15,59 +15,49 @@ public class LNDSGui extends GBFrame {
 
     //Panels
     private GBPanel mainPanel;
-    private GBPanel numbersPanel;
-    private GBPanel lndsPanel;
 
-    //Drop down setup
-    private JMenuItem addNumberMenu;
-    private JMenuItem resetNumbersMenu;
-    private JMenuItem quitMenu;
-
-    //constructor
-    public LNDSGui() {
-
-        //test data
-        //int[] testData = {17, 31, 40, 5, 6, 10, 46, 37, 53, 12, 9, 86};
-        int[] testData = {17, 31, 40, 5, 6, 10, 3, 37, 53, 12, 9, 86};
-        numberArray.addArray(testData);
-
-        //add menu items
-        addNumberMenu = addMenuItem("LNDS","Add New Number to List");
-        resetNumbersMenu = addMenuItem("LNDS","Reset Number List");
-        quitMenu = addMenuItem("LNDS", "Quit");
-    }
+    //Buttons for operations
+    private JButton inputButton;
+    private JButton outputButton;
+    private JButton resetButton;
+    private JButton exitButton;
 
     public void showMainScreen() {
 
-        NumberArray[] lndsArray = LNDSFinder.findLNDS(numberArray);
-        int lndsArraySize = lndsArray==null?0:lndsArray.length;
-        this.setSize (800, 125+25*lndsArraySize);
-
+        this.setSize (600, 125);
         if ( mainPanel != null ) {
             this.remove(mainPanel);
         }
         mainPanel = addPanel(1, 1, 1, 1);
 
-        numbersPanel = mainPanel.addPanel(1, 1, 1, 1);
+        GBPanel numbersPanel = mainPanel.addPanel(1, 1, 1, 1);
         numbersPanel.addLabel("Numbers", 1, 1, 1, 1);
-        numbersPanel.addTextField(""+numberArray, 2, 1, 1, 1).setEnabled(false);
-
-        lndsPanel = mainPanel.addPanel(2, 1, 1, 1);
-        lndsPanel.addLabel("Longest Non-Decreasing Sequences ("+lndsArraySize+")", 1, 1, 1, 1);
-        for ( int i=0; i<lndsArraySize; i++ ) {
-            lndsPanel.addTextField( ""+lndsArray[i], 2+i, 1, 1, 1).setEnabled(false); //read only
+        String numberArrayString = numberArray.toString();
+        if ( numberArrayString.isEmpty() ) {
+            numberArrayString = "no numbers - use the Input button to add";
         }
+        numbersPanel.addTextField(numberArrayString, 2, 1, 1, 1).setEnabled(false);
+
+        GBPanel buttonsPanel = mainPanel.addPanel(2, 1, 1, 1);
+        inputButton = buttonsPanel.addButton("Input", 1, 1, 1, 1);
+        outputButton = buttonsPanel.addButton("Output", 1, 2, 1, 1);
+        resetButton = buttonsPanel.addButton("Reset", 1, 3, 1, 1);
+        exitButton = buttonsPanel.addButton("Exit", 1, 4, 1, 1);
 
         revalidate();
     }
 
     //Method for Drop Down
-    public void menuItemSelected(JMenuItem menuItem) {
-        if ( menuItem == addNumberMenu ) {
-            new AddNumberDialog(this, numberArray).setVisible (true);
-        } else if ( menuItem == resetNumbersMenu ) {
+    public void buttonClicked(JButton buttonObj) {
+        if ( buttonObj == inputButton ) {
+            GBDialog dialog = new InputDialog(this, numberArray);
+            dialog.setVisible(true);
+        } else if ( buttonObj == outputButton ) {
+            GBDialog dialog = new OutputDialog(this, numberArray);
+            dialog.setVisible(true);
+        } else if ( buttonObj == resetButton ) {
             numberArray.reset();
-        } else if ( menuItem == quitMenu ) {
+        } else if ( buttonObj == exitButton ) {
             exit(0);
         }
         showMainScreen();
@@ -82,36 +72,95 @@ public class LNDSGui extends GBFrame {
         gui.setVisible (true);
     }
 
-    public class AddNumberDialog extends GBDialog {
+    public class InputDialog extends GBDialog {
 
         private NumberArray numberArray;
 
-        IntegerField numberField;
+        JTextField numbersField;
         JButton okBtn;
         JButton cancelBtn;
 
-        public AddNumberDialog(JFrame parent, NumberArray numberArray){
+        public InputDialog(JFrame parent, NumberArray numberArray){
 
             super (parent);                                 // ** REQUIRED **
-            setTitle ("Add a Number");
+            setTitle ("Input");
             setDlgCloseIndicator ("Cancel");
             setLocationRelativeTo(null);
-            setSize (300, 200);
+            setSize (400, 100);
 
             // store the number array
             this.numberArray = numberArray;
 
             //add the fields and buttons
-            addLabel ("Nnumber", 1,1,1,1);
-            numberField = addIntegerField (0, 1,2,5,1);
-            okBtn = addButton ("OK", 4,2,2,1);
-            cancelBtn = addButton ("Cancel", 4,4,2,1);
+            addLabel ("Numbers to add (comma separated e.g. 5, 6, 10)", 1,1,4,1);
+            numbersField = addTextField ("", 2,1,4,1);
+            okBtn = addButton ("OK", 3,1,1,1);
+            cancelBtn = addButton ("Cancel", 3,2,1,1);
+        }
+
+        //parse numbers into array to add
+        public int[] getNumbers() throws Exception {
+            String[] numberStrings = numbersField.getText().split(",");
+            int[] numbers = new int[numberStrings.length];
+            for (int i=0; i<numberStrings.length; i++) {
+                numbers[i] = Integer.parseInt(numberStrings[i].trim());
+            }
+            return numbers;
         }
 
         public void buttonClicked(JButton buttonObj) {
             if ( buttonObj == okBtn ) {
-                numberArray.addNumber(numberField.getNumber());
+                try {
+                    numberArray.addArray(getNumbers());
+                } catch (Exception ex) {
+                    messageBox("Error parsing input: " + ex.getMessage());
+                    numbersField.requestFocus();
+                    return;
+                }
             }
+            dispose();
+        }
+    }
+
+    public class OutputDialog extends GBDialog {
+
+        JButton okBtn;
+
+        public OutputDialog(JFrame parent, NumberArray numberArray){
+
+            super (parent);                                 // ** REQUIRED **
+
+            setTitle ("Output");
+            setDlgCloseIndicator ("Cancel");
+            setLocationRelativeTo(null);
+            setSize (600, 100);
+
+            //add the fields and buttons
+            NumberArray[] lndsArray = LNDSFinder.findLNDS(numberArray);
+            int lndsArraySize = lndsArray==null?0:lndsArray.length;
+            if ( lndsArraySize == 0 ) {
+                addLabel("There are no non decreasing sequences", 1, 1, 1, 1);
+            } else {
+                addLabel("Longest Non-Decreasing Sequences (" + lndsArraySize + ")", 1, 1, 1, 1);
+                for (int i = 0; i < lndsArraySize; i++) {
+                    addTextField("" + lndsArray[i], 2+i, 1, 1, 1).setEnabled(false); //read only
+                }
+            }
+            NumberArray[] ldsArray = LNDSFinder.findLDS(numberArray);
+            int ldsArraySize = ldsArray==null?0:ldsArray.length;
+            if ( ldsArraySize == 0 ) {
+                addLabel("There are no decreasing sequences", 2+lndsArraySize, 1, 1, 1);
+            } else {
+                addLabel("Longest Decreasing Sequences (" + ldsArraySize + ")", 2+lndsArraySize, 1, 1, 1);
+                for (int i = 0; i < ldsArraySize; i++) {
+                    addTextField("" + ldsArray[i], 3+lndsArraySize+i, 1, 1, 1).setEnabled(false); //read only
+                }
+            }
+            setSize (600, 100+25*lndsArraySize+25*ldsArraySize);
+            okBtn = addButton ("OK", 4+lndsArraySize+ldsArraySize,1,1,1);
+        }
+
+        public void buttonClicked(JButton buttonObj) {
             dispose();
         }
     }
